86da0950ff215ffb50dad9cfad3e9133e58c39b8
Lucas Souza <souzacontato18@gmail.com>
2022-11-17 16:25:20 -0300

    backend start

---
+++ Backend/ConduitCloneAPI/ConduitCloneAPI/Entities/Article.cs
@@ -0,0 +1,16 @@
+﻿namespace ConduitCloneAPI.Entities
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Theme { get; set; }
+        public string? Body { get; set; }
+        public int Upvotes { get; set; }
+        public int Downvotes { get; set; }
+        // relacionamentos
+        public int UserId { get; set; }
+        public virtual User? User { get; set; }
+        public ICollection<Tag>? Tags { get; set; }
+    }
+}
---
+++ Backend/ConduitCloneAPI/ConduitCloneAPI/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿namespace ConduitCloneAPI.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? Bio { get; set; }
+        public string? ProfilePicture { get;set; }
+        public ICollection<Article>? Articles { get; set; }
+        public ICollection<Article>? LikedArticles { get; set; }
+
+        public User(int id, string? userName, string? password, string? email, string? bio, string? profilePicture)
+        {
+            Id = id;
+            UserName = userName;
+            Password = password;
+            Email = email;
+            Bio = bio;
+            ProfilePicture = profilePicture;
+        }
+    }
+}
